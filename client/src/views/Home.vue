9f5c999bdd41691a7c308f27b652f312a7c69c66
mbuettgenbach <matthias.buettgenbach@cologne.intelligence.de>
2019-04-02 18:58:58 +0200

    added 2 new functions (create and delete)

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="container-fluid">
+        <form @submit.prevent="addExpenditure">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input v-model="expenditure.name" type="text" class="form-control" id="name" aria-describedby="emailHelp" required>
+            </div>
+            <div class="form-group">
+                <label for="amount">Amount</label>
+                <input v-model="expenditure.amount" type="number" class="form-control" id="amount" min="0" step="0.01" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Ok</button>
+        </form>
         <table class="table table-hover" style="marginTop:20px">
             <thead>
                 <tr class="table-light">
@@ -9,7 +20,7 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="table" v-for="e in expenditures" :key="e._id" @dblclick="deleteEntry(e._id)">
+                <tr @dblclick="remove(e)" class="table" v-for="e in expenditures" :key="e._id">
                     <th scope="row">{{e.name}}</th>
                     <td>{{formatNumber(e.amount)}}</td>
                     <td>{{e.created}}</td>
@@ -31,6 +42,10 @@
         name: 'home',
         data: () => ({
             expenditures: [],
+            expenditure: {
+                name: '',
+                amount: ''
+            },
         }),
         computed: {
             total() {
@@ -44,8 +59,25 @@
                     currency: 'EUR'
                 }).format(number)
             },
-            deleteEntry(id) {
-                console.log(`deleting entry with id ${id}`)
+            async addExpenditure() {
+                const response = await fetch(API_URL, {
+                    method: 'POST',
+                    body: JSON.stringify(this.expenditure),
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+                this.expenditures.push(await response.json())
+                this.expenditure.name = ''
+                this.expenditure.amount = ''
+            },
+            async remove(e) {
+                const index = this.expenditures.indexOf(e)
+
+                fetch(`${API_URL}/${e._id}`, {
+                    method: 'DELETE'
+                })
+                this.expenditures.splice(index, 1);
             }
         },
         async mounted() {
